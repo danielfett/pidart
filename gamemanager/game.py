4300e15c73bac6a8be394c6fb73d36464c67c0df
Daniel Fett <fett@danielfett.de>
2013-11-17 12:48:31 +0100

    Add features needed for web interface

--- gamemanager/game.py
+++ gamemanager/game.py
@@ -7,6 +7,11 @@ from sys import exit
 from circuits import Component, Event, Task, Debugger
 import serial
 
+from components.webserver import Webserver
+from components.logger import Logger
+from components.legacysounds import LegacySounds
+from components.espeaksounds import EspeakSounds
+
 class DartInput(object):
     def __init__(self, device = '/dev/ttyUSB0'):
         self.ser = serial.Serial(device, 115200)
@@ -24,7 +29,7 @@ class FakeInput(object):
         inp = raw_input("? ").strip().upper()
         return inp
 
-class GameInitialized(Task):
+class GameInitialized(Event):
     """ A new game was started. """
 
 class DartStuck(Event):
@@ -45,8 +50,11 @@ class HitBust(Event):
 class HitWinner(Event):
     """ A dart hit the board, and the player won. """
 
-class WaitForStart(Event):
+class EnterHold(Event):
     """ Wait for the player to hit start. """
+
+class LeaveHold(Event):
+    """ Player has pressed start to continue. """
 
 class RoundStarted(Event):
     """ A new round was started. """
@@ -57,183 +65,63 @@ class RoundFinished(Event):
 class GameOver(Event):
     """ The game is over. """
 
-""" LOGGING """
-
-from datetime import datetime
-
-class Logger(Component):
-    def game_initialized(self, players):
-        print "Logger started."
-        filename = datetime.now().strftime("%Y-%m-%d--%H:%M.dartlog")
-        self.file = open(filename, 'w')
-        self.file.write("Players: %s\n" % (' '.join(players)))
-
-    def round_finished(self, players, currentplayer, darts):
-        self.file.write("%s: %s\n" % (players[currentplayer], ' '.join(darts)))
-
-""" PLAYING SOUNDS """
-
-import pygame
-
-class LegacySounds(Component):
-    SOUNDS = [
-        'nextplayer',
-        'finish',
-        'winner',
-        'double',
-        'triple',
-        'bullseye',
-        'beep']
-        
-
-    def __init__(self):
-        super(LegacySounds, self).__init__()
-        pygame.init()
-        self.sounds = {}
-        for n in self.SOUNDS:
-            self.sounds[n] = pygame.mixer.Sound('../sounds/old-%s.wav' % n)
-    
-    def play(self, sound):
-        self.sounds[sound].play() 
-
-    def dart_stuck(self, *args):
-        self.play('beep')
-
-    def skip_player(self, *args):
-        self.play('beep')
-
-    def hit(self, players, currentplayer, code):
-        if code == 'D25':
-            self.play('bullseye')
-        elif code.startswith('T'):
-            self.play('triple')
-        elif code.startswith('D'):
-            self.play('double')
-        else:
-            self.play('beep')
-
-    def hit_bust(self, *args):
-        self.play('winner') # sound is missing!
-
-    def hit_winner(self, players, currentplayer, code, winners):
-        self.play('finish')
-        if len(winners) == 0:
-            sleep(3.591)
-            self.play('winner')
-
-    def round_started(self, *args):
-        self.play('nextplayer')
-        
-""" Espeak output """
-
-from espeak import espeak
-
-class EspeakSounds(Component):
-    def __init__(self):
-        super(EspeakSounds, self).__init__()
-        espeak.set_voice('en/en-sc')
-
-    def say(self, text):
-        espeak.synth("%s!" % text)
-
-    def dart_stuck(self, *args):
-        self.say('Dart Stuck.')
-
-    def skip_player(self, *args):
-        self.say('Player skipped.')
-
-    def hit(self, players, currentplayer, code):
-        sleep(0.25)
-        if code == 'S25':
-            self.say('Bull\'s eye')
-        elif code == 'D25':
-            self.say('Inner Bull\'s eye')
-        elif code.startswith('T'):
-            self.say('Triple %s' % code[1:])
-        elif code.startswith('D'):
-            self.say('Triple %s' % code[1:])
-        else:
-            self.say(code[1:])
-
-    def hit_bust(self, *args):
-        self.say('Bust')
-
-    def hit_winner(self, players, currentplayer, code, winners):
-        if len(winners) == 0:
-            self.say('Checked out! You are the winner')
-        else:
-            self.say('Checked out! You are number %d' % (len(winners) + 1))
-
-    def wait_for_start(self, *args):
-        sleep(0.5)
-        self.say('Press START')
-
-    def round_started(self, players, currentplayer, round):
-        self.say('Next player. Is. %s.' % ('.'.join(players[currentplayer])))
-
-""" Websockets connector """
-
-from twisted.internet import reactor
-from twisted.python import log
-from twisted.web.server import Site
-from twisted.web.static import File
-
-from autobahn.websocket import WebSocketServerFactory, \
-                               WebSocketServerProtocol, \
-                               listenWS
- 
-from threading import Thread
-from Queue import Queue
-import simplejson
-
-class DartsProtocol(WebSocketServerProtocol):
-    def __init__(self):
-        self.queue = Queue()
-
-    def onOpen(self):
-        self.factory.register(self)     
-
-class DartsServerFactory(WebSocketServerFactory):
-   def __init__(self, url, debug = False, debugCodePaths = False):
-       WebSocketServerFactory.__init__(self, url, debug = debug, debugCodePaths = debugCodePaths)
-       self.clients = []
-      
-   def broadcast(self, msg):
-       for client in self.clients:
-           client.sendMessage(simplejson.dumps(msg))
-           #client.queue.put(simplejson.dumps(msg))
-       
-
-   def register(self, client):
-       if not client in self.clients:
-           print "registered client " + client.peerstr
-           self.clients.append(client)
-
-   def unregister(self, client):
-       if client in self.clients:
-           print "unregistered client " + client.peerstr
-           self.clients.remove(client)
-
-class WSockets(Component):
-    def __init__(self):
-        super(WSockets, self).__init__()
-        self.factory = DartsServerFactory("ws://localhost:9000")
-
-        self.factory.protocol = DartsProtocol
-        self.factory.setProtocolOptions(allowHixie76 = True)
-        listenWS(self.factory)
-        
-        webdir = File("../html/")
-        web = Site(webdir)
-        reactor.listenTCP(8080, web)
-        self.thread = Thread(target=reactor.run, args=(), kwargs={'installSignalHandlers':0})
-        self.thread.start()
-
-    def hit(self, *args):
-        self.factory.broadcast(args)
-
-
 """ MAIN COMPONENT """
+
+from operator import itemgetter
+
+class GameState(object):
+    def __init__(self, players, startvalue):
+        self.players = players
+        self.scores = {}
+        self.history = {}
+        self.round = 1
+
+        for i in range(len(self.players)):
+            self.scores[i] = startvalue
+            self.history[i] = []
+
+        self.currentPlayer = 0
+        self.currentDarts = []
+
+    def add_dart(self, dart, s):
+        self.currentDarts.append(dart)
+        if self.scores[self.currentPlayer] - s < 0:
+            return 'bust'
+        self.scores[self.currentPlayer] -= s
+        if self.scores[self.currentPlayer] == 0:
+            return 'winner'
+
+    def flush_round(self):
+        self.history[self.currentPlayer].append(self.currentDarts)
+        self.currentDarts = []
+
+        wasPlayer = self.currentPlayer
+        winners = self.winners()
+        while self.currentPlayer == wasPlayer or self.currentPlayer in winners:
+            self.currentPlayer += 1
+            if self.currentPlayer == len(self.players):
+                self.currentPlayer = 0
+                self.round += 1
+        
+    def winners(self):
+        return [x for x in range(len(self.players)) if self.scores[x] == 0]
+
+    def game_over(self):
+        return len(self.winners()) == len(self.players) - 1
+
+    def player_list(self, sortby):
+        lst = [{
+                'started': x, 
+                'rounds': len(self.history[x]), 
+                'last_round': (self.history[x][-1] if len(self.history[x]) else []), 
+                'score': self.scores[x]
+                } for x in range(len(self.players))]
+        s = sorted(lst, key=itemgetter('score', 'rounds', 'started'))
+        for i in range(len(s)):
+            s[i]['rank'] = i
+        if sortby == 'order':
+            return sorted(s, key=itemgetter('started'))
+        return s
 
 class DartGame(Component):
     NUMBEROFDARTS = 3
@@ -261,27 +149,28 @@ class DartGame(Component):
         self.fire(event)
         self.flush()
 
+    def hold(self, manual):
+        self.event(EnterHold(manual))
+        print "Press select or start after removing the darts"
+        while self.input.read() not in ['BSTART', 'BSELECT']:
+            pass
+        self.event(LeaveHold(manual))
+
     def started(self, *args):
-        scores = {}
-        history = {}
-        for i in range(len(self.players)):
-            scores[i] = self.startvalue
-            history[i] = []
+        state = GameState(self.players, self.startvalue)
 
-        currentplayer = 0
-        round = 1
-        darts = []
-        winners = []
-
-        self.event(GameInitialized(self.players))
+        self.event(GameInitialized(state))
 
         while True:
-            darts = []
             wait_for_removal_of_darts = True
-            self.event(RoundStarted(self.players, currentplayer, round))
-            print "=============== Round %d ===============" % round
-            while len(darts) < self.NUMBEROFDARTS:
-                print "--> Now playing Dart #%d in Round %d: %s (%d Points left)" % (len(darts)+1, round, self.players[currentplayer], scores[currentplayer])
+            self.event(RoundStarted(state))
+            print "=============== Round %d ===============" % state.round
+            while len(state.currentDarts) < self.NUMBEROFDARTS:
+                print "--> Now playing Dart #%d in Round %d: %s (%d Points left)" % (
+                    len(state.currentDarts)+1, 
+                    state.round, 
+                    state.players[state.currentPlayer], 
+                    state.scores[state.currentPlayer])
                 code = self.input.read()
                 if code == 'XSTUCK':
                     self.event(DartStuck())
@@ -291,53 +180,43 @@ class DartGame(Component):
                     print "Player skipped."
                     wait_for_removal_of_darts = False
                     break
+                elif code == 'BSELECT':
+                    self.hold(True)
                 elif code.startswith('X') or code.startswith('B'):
                     self.event(CodeNotImplemented())
                     print "Not implemented: %s" % code
                 elif code.startswith('S') or code.startswith('D') or code.startswith('T'):
                     print "Hit %s" % code
                     s = self.score2sum(code)
-                    darts.append(code)
-                    if scores[currentplayer] - s < 0:
-                        self.event(HitBust(self.players, currentplayer, code))
+                    res = state.add_dart(code, s)
+                    if res == 'bust':
+                        self.event(HitBust(state, code))
                         print "BUST!"
                         break 
-                    elif scores[currentplayer] - s == 0:
-                        self.event(HitWinner(self.players, currentplayer, code, winners))
+                    elif res == 'winner':
+                        self.event(HitWinner(state, code))
                         print "WINNER!"
-                        winners.append(currentplayer)
                         break
                     else:
-                        self.event(Hit(self.players, currentplayer, code))
-                        scores[currentplayer] -= s
+                        self.event(Hit(state, code))
 
-            history[currentplayer].append(darts)
-            self.event(RoundFinished(self.players, currentplayer, darts))
+            self.event(RoundFinished(state))
+            state.flush_round()
             
             if wait_for_removal_of_darts:
-                self.event(WaitForStart())
-                print "Press start after removing the darts"
-                while self.input.read() != 'BSTART':
-                    pass
-
-            wasplayer = currentplayer
-            while currentplayer == wasplayer or currentplayer in winners:
-                currentplayer += 1
-                if currentplayer == len(self.players):
-                    currentplayer = 0
-                    round += 1
+                self.hold(False)
 
             # we have n-1 winners, so the game is over and the current player is last
-            if len(winners) == len(self.players) - 1:
-                self.event(GameOver(self.players, winners, history))
+            if state.game_over():
+                self.event(GameOver(state))
                 print "Game is over! Ranking:"
-                for w in winners + [currentplayer]:
-                    print "%s (%d)" % (self.players[w], len(history[w]))
+                for w in state.player_list(sortby='rank'):
+                    print "%d: %s" % (w['rank'] + 1, w['name'])
                 exit()
 
 if __name__ == "__main__":
     #d = DartGame(DartInput(), players)
     #d = (DartGame(DartInput(), players) + Logger() + LegacySounds() + Debugger())
-    d = (DartGame(DartInput(), players) + Logger() + EspeakSounds() + WSockets() + Debugger())
+    d = (DartGame(DartInput(), players) + Logger() + EspeakSounds() + Webserver() + Debugger())
     d.run()
     
--- html/index.js
+++ html/index.js
@@ -16,3 +16,16 @@ $(window).resize(function() {
 $(document).ready(function() {
   fitText()
 });
+
+function DartCtrl($scope) {
+  $scope.currentPlayer = 'AB';
+  $scope.currentScore = '299';
+  $scope.currentDarts = [{text: 'T20'}, {text: 'D1'}];
+  $scope.hold = false;
+  $scope.players = [
+    {rank: 3, name: 'GH', lastround: 'T20 D3 S1', score: 199},
+    {rank: 1, name: 'DH', lastround: 'S1 D3 S1', score: 0}
+  ];
+
+  window.setTimeout(function() { $scope.currentPlayer = '??'; $scope.$apply();}, 2000);
+}
