f09d49479595f71b73b3921193d3a2a576dd24c3
Daniel Fett <fett@uni-trier.de>
2014-01-26 23:27:25 +0100

    Fix serial input

--- gamemanager/receive-test-circuits.py
+++ gamemanager/receive-test-circuits.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python
 
 from circuits import Component, Debugger, Event, handler
-#from circuits.io.serial import Serial
+from circuits.io.serial import Serial
 #from time import sleep
 
 class helloEv(Event):
     """hello Event"""
 
 class SerInput(Component):
-    '''def __init__(self, device = '/dev/ttyUSB0'):
+    def __init__(self, device = '/dev/ttyUSB0'):
         super(SerInput, self).__init__()
         Serial(device).register(self)
     
     def read(self, data):
         print ('Received: %r' % data)
-        self.fire(TestEv('hallo'))'''
+        self.fire(TestEv('hallo'))
 
     def started(self, c):
         self.fire(helloEv())
-        raise SystemExit(0)
+        #raise SystemExit(0)
     
     def helloEv(self):
         print ('test')
 
 if __name__ == "__main__":        
 
-    SerInput(device = '/dev/ttyACM0').run()
+    (SerInput(device = '/dev/ttyUSB0') + Debugger()).run()
     ''' + Debugger() + TT()
     d.run()'''
